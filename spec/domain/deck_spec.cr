45b7e7c9d5b98609ba69898b0308d3c0576ada89
Santiago Bosio <santiago.bosio@gmail.com>
2021-08-07 15:28:22 -0300

    Changed deck to be initialized with 52 valid cards

--- spec/domain/deck_spec.cr
+++ spec/domain/deck_spec.cr
@@ -3,11 +3,11 @@ require "../../src/domain"
 module Domain
   describe Deck do
     context "with a new game" do
-      it "isn't empty" do
+      it "has 52 cards" do
         game = Game.new
         deck = game.deck
 
-        deck.cards.should_not be_empty
+        deck.cards.size.should eq(52)
       end
     end
   end
--- src/domain/deck.cr
+++ src/domain/deck.cr
@@ -2,6 +2,15 @@ require "../domain"
 
 module Domain
   class Deck
-    getter cards = [Card.new("8", "♦️")]
+    getter cards = [] of Card
+
+    def initialize
+      Card::VALID_SUITES.each do |suit|
+        Card::VALID_RANKS.each do |rank|
+          cards.push(Card.new(rank, suit))
+        end
+        cards.shuffle
+      end
+    end
   end
 end
