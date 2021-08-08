2cb0a854478c96caac0bb4a72549db3a08d93a67
Santiago Bosio <santiago.bosio@gmail.com>
2021-08-07 21:34:34 -0300

    Added test for Card equality comparison method

--- spec/domain/card_spec.cr
+++ spec/domain/card_spec.cr
@@ -5,5 +5,27 @@ module Domain
   DUMMY_SUIT = Suit::Diamonds
 
   describe Card do
+    describe "equality comparison" do
+      it "returns `true` if both cards have same rank and suit" do
+        card = Card.new(Rank::Seven, Suit::Hearts)
+        other_card = Card.new(Rank::Seven, Suit::Hearts)
+
+        card.==(other_card).should be_true
+      end
+
+      it "returns `false` if cards differ in rank" do
+        card = Card.new(Rank::Seven, Suit::Hearts)
+        other_card = Card.new(Rank::Two, Suit::Hearts)
+
+        card.==(other_card).should be_false
+      end
+
+      it "returns `false` if cards differ in suit" do
+        card = Card.new(Rank::Seven, Suit::Hearts)
+        other_card = Card.new(Rank::Seven, Suit::Spades)
+
+        card.==(other_card).should be_false
+      end
+    end
   end
 end
