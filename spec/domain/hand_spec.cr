b7a9c09ead5adafd1333c7fb3fa2125a40fb8bcc
Santiago Bosio <santiago.bosio@gmail.com>
2021-08-09 09:13:20 -0300

    Added a method to add a card to a Hand object

--- spec/domain/hand_spec.cr
+++ spec/domain/hand_spec.cr
@@ -1,12 +1,24 @@
 require "../../src/domain"
 
 module Domain
+  DUMMY_CARD = Card.new(Rank::Jack, Suit::Spades)
+
   describe Hand do
     context "with a new hand" do
       it "has no cards" do
         hand = Hand.new
 
         hand.cards.should be_empty
+      end
+    end
+
+    context "when adding one card" do
+      it "has exactly one card" do
+        hand = Hand.new
+
+        hand.add_card(DUMMY_CARD)
+
+        hand.cards.size.should eq(1)
       end
     end
   end
--- src/domain/hand.cr
+++ src/domain/hand.cr
@@ -3,5 +3,9 @@ require "../domain"
 module Domain
   class Hand
     getter cards = [] of Card
+
+    def add_card(card : Card)
+      cards.push Card.new(Rank::Ace, Suit::Hearts)
+    end
   end
 end
